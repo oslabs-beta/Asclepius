54ae705167ae0c47162fb81616cbfbec0470401f
Hugh Stapleton <hstapleton808@gmail.com>
2023-12-19 11:32:26 -0500

    working nodemap

--- client/src/redux/slices/nodeSlice.js
+++ client/src/redux/slices/nodeSlice.js
@@ -26,7 +26,50 @@ export const nodeSlice = createSlice({
                 memPercentage: "51%",
                 color: "green",
                 id: 3
-            }
+            },
+            { 
+                name: "node 2",
+                cpuPercentage: "10%",
+                memPercentage: "51%",
+                color: "green",
+                id: 4
+            },            { 
+                name: "node 2",
+                cpuPercentage: "10%",
+                memPercentage: "51%",
+                color: "green",
+                id: 5
+            },            { 
+                name: "node 2",
+                cpuPercentage: "10%",
+                memPercentage: "51%",
+                color: "green",
+                id: 6
+            },            { 
+                name: "node 2",
+                cpuPercentage: "10%",
+                memPercentage: "51%",
+                color: "green",
+                id: 7
+            },            { 
+                name: "node 2",
+                cpuPercentage: "10%",
+                memPercentage: "51%",
+                color: "green",
+                id: 8
+            },            { 
+                name: "node 2",
+                cpuPercentage: "10%",
+                memPercentage: "51%",
+                color: "green",
+                id: 9
+            },            { 
+                name: "node 2",
+                cpuPercentage: "10%",
+                memPercentage: "51%",
+                color: "green",
+                id: 10
+            },
            ]
     },
     reducers: {

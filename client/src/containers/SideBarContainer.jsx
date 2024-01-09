61f9e46aa8ff35e7ca629e3e687a23aa39252da0
kbamgbosee <kolarocks1@gmail.com>
2024-01-09 12:02:02 -0500

    Sidebar

--- client/src/containers/SideBarContainer.jsx
+++ client/src/containers/SideBarContainer.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import AsclepiusLogo from "./AsclepiusLogo.png";
+import { useSelector } from 'react-redux';
+import Sidebarsection from "../components/Sidebarsection.jsx"
 
 function SideBarContainer() {
+  const sidebarData = useSelector((state) => Object.keys(state.node.sidebarData).length > 0);
+  console.log("should be false", sidebarData)
   return (
     <div id='SideBarContainer'>
      
       <div id="logocontainer">
       <img id="asclepiusLogo" src={AsclepiusLogo} alt="Asclepius logo" />
+      </div>
+      <div id="NodeQuickInfo">
+        {sidebarData ? <Sidebarsection /> : null}
       </div>
     </div>
   );

0c234d93efba80fad9da094e01dd8696c69d0c27
kbamgbosee <kolarocks1@gmail.com>
2024-01-10 16:58:08 -0500

    Final Styling Co-authored-by: John Norlin <johnnorlinmusic@gmail.com> Co-authored-by: Nick Vanderlinden <nick.vanderlinden21@gmail.com> Co-authored-by: Kola Bamgbose <kbamgbose@gmail.com> Co-authored-by: Cam Blair <cameronblair97@gmail.com> Co-authored-by: Hugh Stapleton <hstapleton808@gmail.com>

--- client/src/components/NodeMap.jsx
+++ client/src/components/NodeMap.jsx
@@ -1,7 +1,8 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import * as d3 from 'd3';
 import { setData, setSidebarData } from '../redux/slices/nodeSlice.js';
+
 
 function NodeMap() {
   const dispatch = useDispatch()
@@ -9,10 +10,8 @@ function NodeMap() {
 
   const data = useSelector((state) => state.node.nodes);
   const sidebarData = useSelector((state) => state.node.sidebarData);
+  //const [nodePositions, setNodePositions] = useState(nodes.map(node => ({ x: node.x, y: node.y })));
   //shape of data:
-=======
-   //shape of data:
->>>>>>> ea9007bc837686f06b370f4b2c7c14a421baca22
   // const nodeData = {
   //   name: resultArray[0],
   //   cpuCores: resultArray[1],
@@ -47,7 +46,7 @@ function NodeMap() {
         .catch((err) => {
           console.log(err);
         });
-    }, 5000); // The delay is set to 0, meaning it will be executed in the next event loop cycle
+    }, 5000);
   }, [nodeData]);
 
   //creates a link to the DOM
@@ -68,13 +67,45 @@ function NodeMap() {
   const links = nodes
     .slice(1)
     .map((node) => ({ source: nodes[0], target: node.id }));
-=======
-  console.log(nodes)
-  nodes.unshift({ id: 0, name: "Master Node", color: "grey" });
 
-  const links = nodes.slice(1).map((node) => ({ source: nodes[0], target: node.id }));
->>>>>>> ea9007bc837686f06b370f4b2c7c14a421baca22
-  // console.log("This is links:", links);
+//   const nodes = [
+//     { id: 0, name: 'Node 0'},
+//     { id: 1, name: 'Node 1' },
+//     { id: 2, name: 'Node 2' },
+//     { id: 3, name: 'Node 3' },
+//     { id: 4, name: 'Node 4' },
+//     { id: 5, name: 'Node 5' },
+//     { id: 6, name: 'Node 6' },
+//     { id: 7, name: 'Node 7' },
+//     { id: 8, name: 'Node 8' },
+//     { id: 9, name: 'Node 9' },
+//     { id: 10, name: 'Node 10' },
+//     { id: 11, name: 'Node 11' },
+//     { id: 12, name: 'Node 12' }, 
+//     { id: 13, name: 'Node 13' },
+//     { id: 14, name: 'Node 14' },
+//     { id: 15, name: 'Node 15' },
+//     { id: 16, name: 'Node 16' },
+// ];
+
+// const links = [
+//     { source: 0, target: 1 },
+//     { source: 0, target: 2 },
+//     { source: 0, target: 3 },
+//     { source: 0, target: 4 },
+//     { source: 0, target: 5 },
+//     { source: 0, target: 6 },
+//     { source: 0, target: 7 },
+//     { source: 0, target: 8 },
+//     { source: 0, target: 9 },
+//     { source: 0, target: 10 },
+//     { source: 0, target: 11 },
+//     { source: 0, target: 12 },
+//     { source: 0, target: 13 },
+//     { source: 0, target: 14 },
+//     { source: 0, target: 15 },
+//     { source: 0, target: 16 },
+// ];
 
   const width = 200;
   const height = 200;
@@ -93,10 +124,7 @@ function NodeMap() {
         'transform',
         'translate(' + width / 1.25 + ',' + 500 + ')rotate(-70)'
       );
-=======
-      .append("g")
-      .attr("transform", "translate(" + width / 1.65 + "," + height / 2 + ")");
->>>>>>> ea9007bc837686f06b370f4b2c7c14a421baca22
+
 
     const simulation = d3
       .forceSimulation(nodes)
@@ -119,16 +147,11 @@ function NodeMap() {
       .attr('class', 'link')
       .style('stroke', 'black')
       .attr('opacity', 1);
-=======
-      .append("line")
-      .attr("class", "link") 
-      .style("stroke", "black")
-      .attr("opacity", 1);
->>>>>>> ea9007bc837686f06b370f4b2c7c14a421baca22
+
 
     //changes the radius of nodes depending on number of nodes rendered
-    const scale = Math.min(70, 280 / nodes.length);
-    const maxFontSize = scale * 0.8;
+    const scale = Math.min(70, 7000 / nodes.length);
+    const maxFontSize = scale * 0.6;
     // Create nodes
     const node = group
       .selectAll('.node')
@@ -137,6 +160,7 @@ function NodeMap() {
       .append('circle')
       .attr('class', 'node')
       .attr('r', scale)
+      .style('stroke', 'black')
       .attr('fill', (d) => d.color)
       .on('click', function (event, d) {
         //call helper function
@@ -152,6 +176,10 @@ function NodeMap() {
       .attr('class', 'label')
       .attr('dy', 4)
       .attr('text-anchor', 'middle')
+      .style('font-family', 'Play, sans-serif')
+      .style('font-size', () => {
+        return Math.min(maxFontSize, 10);
+      })
       .text((d) => d.name);
 
 //       nodes.forEach((d, i) => {
@@ -160,6 +188,7 @@ function NodeMap() {
 // });
 
     function ticked() {
+
       link
         .attr('x1', (d) => d.source.x)
         .attr('y1', (d) => d.source.y)
@@ -170,11 +199,8 @@ function NodeMap() {
 
       label.attr('x', (d) => d.x).attr('y', (d) => d.y);
       label.attr('transform', (d) => `rotate(70, ${d.x}, ${d.y})`)
-      label.style('font-size', () => {
-        return Math.min(maxFontSize, 10);
-      })
     }
-  }, [nodes, links, dispatch]);
+  }, [nodes, links, dispatch]);//positions
 
   return (
     <div id='innerNodeMapContainer'>

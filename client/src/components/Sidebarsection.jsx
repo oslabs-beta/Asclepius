c8ef6fba2d2118e56dc69f7fa7c83b97eb891500
Nick <nick.vanderlinden21@gmail.com>
2024-03-14 12:36:30 -0400

    Updated styling and sidebar Co-authored-by: Hugh Stapleton <hstapleton808@gmail.com>

--- client/src/components/Sidebarsection.jsx
+++ client/src/components/Sidebarsection.jsx
@@ -1,38 +1,43 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 function Sidebarsection() {
   const data = useSelector((state) => state.node.sidebarData);
   const name = useSelector((state) => state.node.clusterName);
 
   let nodeName;
-  data.name ? (nodeName = data.name.split("-")) : null;
+  data.name ? (nodeName = data.name.split('-')) : null;
 
   return (
-    <div id="sideBar">
-      <div id="cluster">
+    <div id='sideBar'>
+      <div id='cluster'>
         <div>Cluster Name:</div>
-        <div>{name ? name : null}</div>{" "}
+        <div className='connectedDataText'>{name ? name : null}</div>{' '}
       </div>
-      <div id="small">
+      <div id='small'>
         <table>
           <thead>
             <tr>
               <th>Node Name:</th>
               <td></td>
-              <td>{nodeName ? nodeName[3] : null}</td>
+              <td className='connectedDataText'>
+                {nodeName ? nodeName[3] : null}
+              </td>
             </tr>
           </thead>
         </table>
       </div>
-      <div id="cpu">
+      <div id='cpu'>
         <div>
           <table>
             <thead>
               <tr>
                 <th>CPU:</th>
                 <td></td>
-                <td> {data ? data.cpuPercentage : null}</td>
+                <td className='connectedDataText'>
+                  {' '}
+                  {data ? data.cpuPercentage : null}
+                </td>
               </tr>
             </thead>
           </table>
@@ -43,20 +48,24 @@ function Sidebarsection() {
               <tr>
                 <th>CPU Cores:</th>
                 <td></td>
-                <td>{data ? data.cpuCores : null}</td>
+                <td className='connectedDataText'>
+                  {data ? data.cpuCores : null}
+                </td>
               </tr>
             </thead>
           </table>
         </div>
       </div>
-      <div id="mem">
+      <div id='mem'>
         <div>
           <table>
             <thead>
               <tr>
                 <th>Memory:</th>
                 <td></td>
-                <td>{data ? data.memPercentage : null}</td>
+                <td className='connectedDataText'>
+                  {data ? data.memPercentage : null}
+                </td>
               </tr>
             </thead>
           </table>
@@ -67,16 +76,18 @@ function Sidebarsection() {
               <tr>
                 <th>Memory Bytes:</th>
                 <td></td>
-                <td>{data ? data.memBytes : null}</td>
+                <td className='connectedDataText'>
+                  {data ? data.memBytes : null}
+                </td>
               </tr>
             </thead>
           </table>
         </div>
       </div>
-      <div id="big">
+      <div id='big'>
         <div>Pods:</div>
         <div>
-          <ul id="list">
+          <ul id='list' className='connectedDataText'>
             {data.pods
               ? data.pods.map((pod, index) => <li key={index}>{pod}</li>)
               : null}
